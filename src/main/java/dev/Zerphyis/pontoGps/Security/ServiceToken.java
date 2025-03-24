3b93b54f9f84e4c88932250f4c2b205525f51e40
Otavio <estudosprogotavio@gmail.com>
2025-03-24 17:47:30 -0300

    Adicionando Service de criação do token JWT

---
+++ src/main/java/dev/Zerphyis/pontoGps/Security/ServiceToken.java
@@ -0,0 +1,51 @@
+package dev.Zerphyis.pontoGps.Security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import dev.Zerphyis.pontoGps.Entitys.Login;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class ServiceToken {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+
+    public String generateToken(Login user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("pontoGps")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar token", exception);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            DecodedJWT decodedJWT = JWT.require(algorithm)
+                    .withIssuer("pontoGps")
+                    .build()
+                    .verify(token);
+            return decodedJWT.getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
+    private Instant genExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
