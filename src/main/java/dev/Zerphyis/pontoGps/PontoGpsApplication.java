1cb16a95ed5a35aa901d9686bef69125dc91fb55
Otavio <estudosprogotavio@gmail.com>
2025-03-24 17:26:51 -0300

    commit inicial

---
+++ src/main/java/dev/Zerphyis/pontoGps/PontoGpsApplication.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.pontoGps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoGpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoGpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Zerphyis/pontoGps/PontoGpsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.pontoGps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoGpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
